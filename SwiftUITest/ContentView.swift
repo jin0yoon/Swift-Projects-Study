22d470b4fccb8e0d1b280a691eac51d964dcce0e
Yoon Jinyeong <wwwz4805@naver.com>
2020-09-30 10:10:22 +0900

    Initial commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by 윤진영 on 2020/09/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+           
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by 윤진영 on 2020/09/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct SwiftUITestApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
