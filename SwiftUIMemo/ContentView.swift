211f41e77f6fb9a98975d03f043f83d94fb3e6f4
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-06 14:14:44 +0900

    Initial Commit

---
+++ SwiftUIMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by 윤진영 on 2020/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
