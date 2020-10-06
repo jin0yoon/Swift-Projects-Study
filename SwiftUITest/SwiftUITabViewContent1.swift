af3e31594bb0ee10fa542c6faa4de651e531bf1f
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-06 12:54:38 +0900

    Create TabView

---
+++ SwiftUITest/SwiftUITabViewContent1.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUITabViewContent1.swift
+//  SwiftUITest
+//
+//  Created by 윤진영 on 2020/10/06.
+//
+
+import SwiftUI
+
+struct SwiftUITabViewContent1: View {
+    var body: some View {
+        Text("Menu Tab")
+    }
+}
+
+struct SwiftUITabViewContent1_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUITabViewContent1()
+    }
+}
