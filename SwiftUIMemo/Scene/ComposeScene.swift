ed82e245dda46aa8bb3ce59c6ab7b959147febce
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-10 18:17:10 +0900

    Update list -> compose
    list에서 메모 작성으로 화면 이동

--- SwiftUIMemo/Scene/ComposeScene.swift
+++ SwiftUIMemo/Scene/ComposeScene.swift
@@ -13,6 +13,8 @@ struct ComposeScene: View {
     //입력한 텍스트를 바인딩하는데 사용하는 속성 선언 -> 주로 State 속성으로 선언
     @State private var content: String = ""
     
+    @Binding var showComposer: Bool
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -28,7 +30,7 @@ struct ComposeScene: View {
             .navigationBarTitle("새 메모", displayMode: .inline)   //largeTitle 모드를 비활성화
             
             //버튼을 생성자만 호출하여 바로 전달
-            .navigationBarItems(leading: DismissButton(), trailing: SaveButton())
+            .navigationBarItems(leading: DismissButton(show: $showComposer), trailing: SaveButton(show: $showComposer))
         }
     }
 }
@@ -36,9 +38,11 @@ struct ComposeScene: View {
 //코드의 가독성을 위해 Button을 별도의 view로 빼서 파라미터로 전달하는 부분을 최대한 간단하게 작성해야 함
 //취소 버튼
 fileprivate struct DismissButton: View{
+    @Binding var show: Bool
+    
     var body: some View{
         Button(action: {
-            
+            self.show = false
         }, label: {
             Text("취소")
         })
@@ -46,9 +50,11 @@ fileprivate struct DismissButton: View{
 }
 //저장 버튼
 fileprivate struct SaveButton: View{
+    @Binding var show: Bool
+    
     var body: some View{
         Button(action: {
-            
+            self.show = false
         }, label: {
             Text("저장")
         })
@@ -57,7 +63,10 @@ fileprivate struct SaveButton: View{
 
 struct ComposeScene_Previews: PreviewProvider {
     static var previews: some View {
-        ComposeScene()
+        //preview에서 에러가 발생하지 않도록 생성자로 속성을 전달
+        //여기로 전달할 수 있는 속성이 없음
+        //그런경우 보통 constant 바인딩을 전달
+        ComposeScene(showComposer: .constant(false))
             //MemoStore를 커스텀 공유데이터로 등록
             .environmentObject(MemoStore())
     }
