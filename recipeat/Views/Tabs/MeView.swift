eb197283b9e72e2d74238e866ae8a68520f9ca8e
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-11 16:36:51 +0900

    Create Tabs Folder and Tab files

---
+++ recipeat/Views/Tabs/MeView.swift
@@ -0,0 +1,20 @@
+//
+//  MeView.swift
+//  recipeat
+//
+//  Created by 윤진영 on 2020/10/11.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
---
+++ recipeat/Views/Tabs/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  recipeat
+//
+//  Created by 윤진영 on 2020/10/11.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
