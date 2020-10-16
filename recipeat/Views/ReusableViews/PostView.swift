cba6bf5670d5baa891db2b573cc7687ff44caf97
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-16 19:01:39 +0900

    Change folders

--- recipeat/Views/ReusableViews/PostView.swift
+++ recipeat/Views/ReusableViews/PostView.swift
@@ -14,7 +14,7 @@ struct PostView: View {
             Image(systemName: "heart")
                 .frame(height: 300)
                 .frame(minWidth: 0, maxWidth: .infinity)
-                .background(Color.init(red: 0.95, green: 0.95, blue: 0.95))
+                .background(Color.init(red: 0.95, green: 0.95, blue: 0.95).opacity(0.6))
             VStack {
                 HStack{
                     Image(systemName: "heart")
@@ -32,9 +32,10 @@ struct PostView: View {
                     Spacer()
                 }.font(.system(size: 12, weight: .regular))
             }.padding()
+            .background(Color.orange)
             
             
-        }
+        }.background(Color.yellow)
     }
 }
 
