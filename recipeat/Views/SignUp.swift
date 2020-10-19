41aaf94fbba7876cd64aeffd489c966f90d21871
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-19 14:20:46 +0900

    Update SignUp.swift

--- recipeat/Views/SignUp.swift
+++ recipeat/Views/SignUp.swift
@@ -15,13 +15,59 @@ struct SignUp: View {
     @State private var email:String = ""
     
     var body: some View {
-        VStack{
-            Text("Sign up")
-            TextField("Name", text: $name)
-            TextField("Email", text: $email)
-            TextField("Username", text: $username)
-            TextField("Password", text: $password)
-            
+        ZStack {
+            Image("fadeCarousel_2_blur")
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width)
+                .edgesIgnoringSafeArea(.all)
+            ZStack{
+                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .leading, endPoint: .trailing).opacity(0.2)
+                LinearGradient(gradient: Gradient(colors: [Color.black, Color.white]), startPoint: .top, endPoint: .bottom).opacity(0.2)
+                Color.white.opacity(0.1)
+            }.edgesIgnoringSafeArea(.all)
+            VStack(spacing:0){
+                Image("recipeat_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(radius: 5, y: 5)
+                Spacer().frame(height:50)
+
+                VStack(spacing:0){
+                    TextField("Name", text: $name)
+                        .frame(height:40)
+                    Rectangle().frame(height:2).foregroundColor(.white)
+                        .opacity(0.2)
+                    
+                    TextField("Email", text: $email)
+                        .frame(height:40)
+                    Rectangle().frame(height:2).foregroundColor(.white)
+                        .opacity(0.2)
+                    
+                    TextField("Username", text: $username)
+                        .frame(height:40)
+                    Rectangle().frame(height:2).foregroundColor(.white)
+                        .opacity(0.2)
+                    
+                    TextField("Password", text: $password)
+                        .frame(height:40)
+                    Rectangle().frame(height:2).foregroundColor(.white)
+                        .opacity(0.2)
+                }
+                
+                Spacer().frame(height:50)
+                
+                Button(action: {
+                    print("here")
+                }) {
+                    HStack{
+                        Text("Sign up")
+                        Image(systemName: "checkmark")
+                    }
+                    
+                }
+                Spacer().frame(height:250)
+            }.padding(30)
         }
     }
 }
