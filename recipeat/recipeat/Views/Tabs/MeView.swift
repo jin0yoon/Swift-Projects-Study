f43f7aa4bd6e9ea2d153e1bdeb6b4b617615caa4
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-02 17:11:54 +0900

    Update MeView.swift

--- recipeat/recipeat/Views/Tabs/MeView.swift
+++ recipeat/recipeat/Views/Tabs/MeView.swift
@@ -55,10 +55,12 @@ struct MeView: View {
                 }
                 
                 ScrollView{
-                    ForEach(0..<env.currentUser.publishedRecipes.count){ i in
-                        
-                        Me_PostView()
-                    }
+                    VStack(spacing:0) {
+                        ForEach(0..<env.currentUser.publishedRecipes.count){ i in
+                            
+                            Me_PostView()
+                        }
+                    }.background(Color.red)
                 }
             }
             
