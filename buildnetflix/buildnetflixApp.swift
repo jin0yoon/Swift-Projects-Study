e75f4b2b303acb8867baae4100dd848444715134
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-10 20:15:35 +0900

    Initial commit

---
+++ buildnetflix/buildnetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  buildnetflixApp.swift
+//  buildnetflix
+//
+//  Created by 윤진영 on 2020/11/10.
+//
+
+import SwiftUI
+
+@main
+struct buildnetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
