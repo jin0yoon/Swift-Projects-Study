c5dcb4dd051058d4681f1a20c2bbf02dbfa73638
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-15 21:24:40 +0900

    Create iOS target

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by 윤진영 on 2020/11/11.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
