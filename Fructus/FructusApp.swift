7b7da5797420b49936289f87c019f42731b5dcd6
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-18 12:51:32 +0900

    Initial commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 윤진영 on 2020/11/18.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
