6f9e9cf0b27cf1f5fac22e7831a3763041570e9a
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-25 15:09:51 +0900

    Update Asset files

---
+++ SwiftUITest/ComplexAnimations/View/ComplexAnimationHome.swift
@@ -0,0 +1,27 @@
+//
+//  ComplexAnimationHome.swift
+//  SwiftUITest
+//
+//  Created by 윤진영 on 2020/11/23.
+//
+
+import SwiftUI
+
+struct ComplexAnimationHome: View {
+    var body: some View {
+        
+        VStack{
+            
+            HStack{
+                
+                Button
+            }
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ComplexAnimationHome()
+    }
+}
--- SwiftUITest/SwiftUITestApp.swift
+++ SwiftUITest/SwiftUITestApp.swift
@@ -11,13 +11,7 @@ import SwiftUI
 struct SwiftUITestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ComplexAnimationHome()
         }
-    }
-}
-
-struct SwiftUITestApp_Previews: PreviewProvider {
-    static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
