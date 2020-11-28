cc0931a04141f72dd799b399bfa9a5aa321bd624
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-29 08:50:09 +0900

    Initial commit of Africa

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by 윤진영 on 2020/11/29.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
