b3473b6ea2febe8f26b6eb7c8affbd55cdaa52c5
Yoon Jinyeong <wwwz4805@naver.com>
2020-12-16 16:10:41 +0900

    Initial commit

---
+++ SwiftUICoreData/SwiftUICoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoreDataApp.swift
+//  SwiftUICoreData
+//
+//  Created by 윤진영 on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
