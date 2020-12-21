f288231ef03b14c675189626d10b57be02ccdb98
Yoon Jinyeong <wwwz4805@naver.com>
2020-12-21 18:56:23 +0900

    Update CoreDataManager

--- SwiftUICoreData/SwiftUICoreDataApp.swift
+++ SwiftUICoreData/SwiftUICoreDataApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct SwiftUICoreDataApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, CoreDataManager.shared.persistentContainer.viewContext)
+                
         }
     }
+    
 }
