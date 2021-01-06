f58fec5edbe88b0d68acf6c2ee5ac542d51cdddf
Yoon Jinyeong <wwwz4805@naver.com>
2021-01-06 17:04:26 +0900

    Initial Commit SwiftUIPencilkit

---
+++ SwiftUIPencikitTest/SwiftUIPencikitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPencikitTest
+//
+//  Created by 윤진영 on 2021/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPencikitTest/SwiftUIPencikitTest/SwiftUIPencikitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPencikitTestApp.swift
+//  SwiftUIPencikitTest
+//
+//  Created by 윤진영 on 2021/01/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPencikitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
