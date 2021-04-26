e5529e9f772ed387f24edb2e81c4b3f38703049f
Yoon Jinyeong <wwwz4805@naver.com>
2021-04-26 20:08:44 +0900

    Create NetflixClone

---
+++ SwiftUI/Project-netflix-clone/NetflixClone/Shared/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  NetflixClone
+//
+//  Created by 윤진영 on 2021/04/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ SwiftUI/Project-netflix-clone/NetflixClone/Shared/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  Shared
+//
+//  Created by 윤진영 on 2021/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
