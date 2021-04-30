6648bd137450eb0afdba0efaffcc88e55b665703
Yoon Jinyeong <wwwz4805@naver.com>
2021-04-30 20:04:57 +0900

    (NetflixClone) Update GlobalHelpers, KFImage

---
+++ SwiftUI/Project-netflix-clone/NetflixClone/iOS/GlobalHelpers.swift
@@ -0,0 +1,10 @@
+//
+//  GlobalHelpers.swift
+//  NetflixClone (iOS)
+//
+//  Created by 윤진영 on 2021/04/27.
+//
+
+import Foundation
+
+let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!)
--- SwiftUI/Project-netflix-clone/NetflixClone/iOS/StandardHomeMovie.swift
+++ SwiftUI/Project-netflix-clone/NetflixClone/iOS/StandardHomeMovie.swift
@@ -6,18 +6,19 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct StandardHomeMovie: View {
     
     var movie: Movie
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        KFImage(movie.thumbnailURL)
     }
 }
 
 struct StandardHomeMovie_Previews: PreviewProvider {
     static var previews: some View {
-        StandardHomeMovie(movie: Movie(id: <#T##String#>, name: <#T##String#>, thumbnailURL: <#T##URL#>))
+        StandardHomeMovie(movie: exampleMovie1)
     }
 }
