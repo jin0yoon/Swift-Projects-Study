4848e2bbd8100253919637bf7f9099a3294de5ca
yoonjinyeong <wwwz4805@kakao.com>
2021-05-05 18:50:37 +0900

    Create TouchDown Project

---
+++ SwiftUI/TouchDown/TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by 윤진영 on 2021/05/05.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
