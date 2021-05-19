25e729361e0bebc8949fa2c08d1367f3d0723a51
Yoon Jinyeong <wwwz4805@kakao.com>
2021-05-19 17:21:30 +0900

    [PetStar] Init project

---
+++ SwiftUI/PetStar/PetStar/PetStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetStarApp.swift
+//  PetStar
+//
+//  Created by 윤진영 on 2021/05/19.
+//
+
+import SwiftUI
+
+@main
+struct PetStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
