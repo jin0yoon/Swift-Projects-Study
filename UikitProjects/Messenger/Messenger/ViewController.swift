c4b0db6c636d8281ef52eeaa6bdebd29beea78ab
Yoon Jinyeong <wwwz4805@kakao.com>
2021-05-20 16:17:24 +0900

    [UIKit - Messenger] Init files

---
+++ UikitProjects/Messenger/Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by 윤진영 on 2021/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
