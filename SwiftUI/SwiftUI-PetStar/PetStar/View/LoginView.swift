073c88021da56a2de580f7cf47eb204e6336317f
Yoon Jinyeong <wwwz4805@kakao.com>
2021-05-22 22:01:00 +0900

    [SwiftUI-PetStar] Update file structure, Create LoginView

---
+++ SwiftUI/SwiftUI-PetStar/PetStar/View/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  PetStar
+//
+//  Created by 윤진영 on 2021/05/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
