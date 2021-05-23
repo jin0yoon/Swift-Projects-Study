e8ccfd9a3213e1873d972678d566f0b463bacd54
Yoon Jinyeong <wwwz4805@kakao.com>
2021-05-23 16:14:20 +0900

    [SwiftUI-AppleSwiftUITutorial] Init project

---
+++ SwiftUI/AppleSwiftUITutorial/AppleSwiftUITutorial/AppleSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSwiftUITutorialApp.swift
+//  AppleSwiftUITutorial
+//
+//  Created by 윤진영 on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
