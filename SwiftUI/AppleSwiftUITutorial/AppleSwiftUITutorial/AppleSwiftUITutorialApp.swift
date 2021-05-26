4240796ce84eacc2e13a1996e573a1bac4237b14
Yoon Jinyeong <wwwz4805@kakao.com>
2021-05-26 15:29:50 +0900

    [SwiftUI-AppleSwiftUITutorial] Update Section1 Explore the Canvas, Section2 Customize the Text View

--- SwiftUI/AppleSwiftUITutorial/AppleSwiftUITutorial/AppleSwiftUITutorialApp.swift
+++ SwiftUI/AppleSwiftUITutorial/AppleSwiftUITutorial/AppleSwiftUITutorialApp.swift
@@ -5,6 +5,12 @@
 //  Created by 윤진영 on 2021/05/23.
 //
 
+/*
+ An app that uses the SwiftUI app life cycle has a structure that conforms to the App protocol.
+ The structure's body property returns one or more scenes, which in turn provide content for display.
+ The @main attribute identifies the app's entry point.
+ */
+
 import SwiftUI
 
 @main
--- SwiftUI/AppleSwiftUITutorial/README.md
+++ SwiftUI/AppleSwiftUITutorial/README.md
@@ -4,6 +4,11 @@
 ## SwiftUI Essentials
 
 ### Creating and Combining Views
+- 이 튜토리얼에서는 좋아하는 장소를 검색하고 공유하기 위한 앱인 랜드마크 구축에 대해 설명합니다.
+- 랜드마크의 세부 정보를 보여주는 view를 구축하는 것으로 시작합니다.
+
+- view를 배치하기 위해 stack을 사용하여 이미지 및 텍스트 view를 결합합니다.
+- Mapkit을 사용하여 지도를 추가합니다.
 
 ### Building Lists and Navigation
 
