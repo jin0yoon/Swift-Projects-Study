7370fa9f28163b39db13ce4c56995b120b71b3dc
Yoon Jinyeong <wwwz4805@kakao.com>
2021-07-18 13:18:31 +0900

    [SwiftUIPrototypingPlaygroundBookBaseCodes] Create Project

---
+++ SwiftUI/SwiftUIPrototypingPlaygroundBookBaseCodes/SwiftUIPrototypingPlaygroundBookBaseCode/SwiftUIPrototypingPlaygroundBookBaseCode/SwiftUIPrototypingPlaygroundBookBaseCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPrototypingPlaygroundBookBaseCodeApp.swift
+//  SwiftUIPrototypingPlaygroundBookBaseCode
+//
+//  Created by 윤진영 on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPrototypingPlaygroundBookBaseCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
